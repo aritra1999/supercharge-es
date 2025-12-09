4ce1ee24b4b50ffd2833529336ddf71df6e9ecb0
Aritra Mondal <aritra041999.dev@gmail.com>
2025-12-09 23:11:40 +0100

    basic golang gin router

--- basic-es/main.go
+++ basic-es/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
+
+func PingController(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
 
 func main() {
-	fmt.Println("Hello, ES Supercharged with LLM!")
+	router := gin.Default()
+
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	router.GET("/ping", PingController)
+
+	router.Run(":8000")
 }
