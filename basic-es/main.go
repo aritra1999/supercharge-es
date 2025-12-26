5f0e2e1f49a7020c559ba9f95547051ddc57f9ba
Aritra Mondal <aritra041999.dev@gmail.com>
2025-12-26 23:01:36 +0100

    add es health controller and route

--- basic-es/main.go
+++ basic-es/main.go
@@ -2,20 +2,16 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func PingController(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "pong",
-	})
-}
-
 func main() {
 	router := gin.Default()
+
+	ConnectES()
 
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
-	router.GET("/ping", PingController)
+	router.GET("/health", HealthController)
 
 	router.Run(":8000")
 }
